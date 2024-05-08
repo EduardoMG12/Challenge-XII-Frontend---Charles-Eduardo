07347e73c1664c4181d050e3cc4c602acc5f2249
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2024-05-08 15:13:26 -0300

    refactor: modify header and footer remove components icons and add link icons to aws S3

--- src/components/outlet/footer.tsx
+++ src/components/outlet/footer.tsx
@@ -1,27 +1,40 @@
-import Facebook from "../icons/facebook";
-import Instagram from "../icons/instagram";
-import LogoFooterMyRide from "../icons/logoFooterMyRide";
-import Twitter from "../icons/twitter";
-
 const Footer = () => {
     return (
         <footer className="h-60 max-w-full  px-20 py-10 flex items-center sm:items-start gap-20 flex-col sm:flex-row mb-10">
             <div className="flex flex-col justify-center sm:justify-start items-center sm:items-start gap-4">
                 <a href="/">
-                    <LogoFooterMyRide />
+                    <img
+                        src="https://challengeuolpbcompass.s3.amazonaws.com/Outlet/logoIcon/LogoMyRideFillWhite.svg"
+                        alt="Logo in the shape of an isosceles triangle with slightly rounded edges, entirely in white. The logo is inscribed within a circle. The word ‘my’ is written in the upper part of the circle, and below ‘my’, within the isosceles triangle that lacks a background, the word ‘ride’ is written."
+                    />
                 </a>
                 <p className="text-[#B3B3B3]">
                     MyRide Inc., 2nd Floor, New York, NY 10016
                 </p>
                 <div className="flex gap-6">
-                    <a href="https://www.facebook.com/compass.uol/" target="_blank">
-                        <Facebook />
+                    <a
+                        href="https://www.facebook.com/compass.uol/"
+                        target="_blank"
+                    >
+                        <img
+                            src="https://challengeuolpbcompass.s3.amazonaws.com/Outlet/socialIcons/Facebook.svg"
+                            alt="facebook icon with background full rounded white"
+                        />
                     </a>
-                    <a href="https://www.instagram.com/compass.uol/" target="_blank">
-                        <Instagram />
+                    <a
+                        href="https://www.instagram.com/compass.uol/"
+                        target="_blank"
+                    >
+                        <img
+                            src="https://challengeuolpbcompass.s3.amazonaws.com/Outlet/socialIcons/Instagram.svg"
+                            alt="instagram icon with background full rounded white"
+                        />
                     </a>
                     <a href="https://twitter.com/compassuol" target="_blank">
-                        <Twitter />
+                        <img
+                            src="https://challengeuolpbcompass.s3.amazonaws.com/Outlet/socialIcons/Twitter.svg"
+                            alt="twitter icon with background full rounded white"
+                        />
                     </a>
                 </div>
             </div>
@@ -33,7 +46,9 @@ const Footer = () => {
                         <li className="text-base cursor-pointer">About Us</li>
                         <li className="text-base cursor-pointer">News</li>
                         <li className="text-base cursor-pointer">Careers</li>
-                        <li className="text-base cursor-pointer">How we work</li>
+                        <li className="text-base cursor-pointer">
+                            How we work
+                        </li>
                     </ul>
                 </div>
 
@@ -43,8 +58,12 @@ const Footer = () => {
                     <ul className="flex flex-col sm:w-44 gap-3">
                         <li className="text-base cursor-pointer">FAQ</li>
                         <li className="text-base cursor-pointer">US Office</li>
-                        <li className="text-base cursor-pointer">Asia Office</li>
-                        <li className="text-base cursor-pointer">Help Center</li>
+                        <li className="text-base cursor-pointer">
+                            Asia Office
+                        </li>
+                        <li className="text-base cursor-pointer">
+                            Help Center
+                        </li>
                     </ul>
                 </div>
 
@@ -52,9 +71,15 @@ const Footer = () => {
                     <p className="text-xl gap-5">More</p>
 
                     <ul className="flex flex-col gap-3">
-                        <li className="text-base cursor-pointer">Become a partner</li>
-                        <li className="text-base cursor-pointer">Partner Support</li>
-                        <li className="text-base cursor-pointer">Mobile app links</li>
+                        <li className="text-base cursor-pointer">
+                            Become a partner
+                        </li>
+                        <li className="text-base cursor-pointer">
+                            Partner Support
+                        </li>
+                        <li className="text-base cursor-pointer">
+                            Mobile app links
+                        </li>
                     </ul>
                 </div>
             </div>
--- src/components/outlet/header.tsx
+++ src/components/outlet/header.tsx
@@ -1,13 +1,13 @@
-import LogoMyRide from "../icons/logoHeaderMyRide";
-import Notify from "../icons/notify";
-
 const Header = () => {
     return (
         <header className=" min-h-10 min-w-full max-w-full my-5 mb-5">
             <div className="px-5 w-full flex flex-col sm:flex-row justify-between items-center gap-5 md:gap-0">
                 <div className="">
                     <a href="/">
-                        <LogoMyRide style={{ cursor: "pointer" }} />
+                        <img
+                            src="https://challengeuolpbcompass.s3.amazonaws.com/Outlet/logoIcon/LogoMyRideFillYellow.svg"
+                            alt="Logo in the shape of an isosceles triangle with slightly rounded edges, entirely in yellow. The logo is inscribed within a circle. The word ‘my’ is written in the upper part of the circle, and below ‘my’, within the isosceles triangle that lacks a background, the word ‘ride’ is written."
+                        />
                     </a>
                 </div>
                 <ul className="flex flex-col sm:flex-row items-center justify-around gap-7">
@@ -26,13 +26,10 @@ const Header = () => {
                     </li>
                 </ul>
                 <div className="flex items-center gap-5">
-                    <Notify style={{ cursor: "pointer" }} />
+                    <img src="https://challengeuolpbcompass.s3.amazonaws.com/Outlet/bellNotifyIcon.svg" alt="A notification bell icon, entirely in white, with a yellow circle located at its upper right corner." className="cursor-pointer"/>
+                    
                     <div className="w-[0.01rem] h-9 bg-[#333333]"></div>
-                    <div className="w-9 h-9 rounded-full bg-[#FBA403] cursor-pointer">
-                        <p className="text-xs text-cyan-800 text-center">
-                            API image
-                        </p>
-                    </div>
+                    <img src="https://challengeuolpbcompass.s3.amazonaws.com/Outlet/userAvatar.png" alt="Avata image with borders full rounded." className="cursor-pointer"/>
                 </div>
             </div>
         </header>
