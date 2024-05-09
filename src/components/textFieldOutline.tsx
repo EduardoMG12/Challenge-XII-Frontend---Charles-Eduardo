7969f81a4390835f18f1949e49cf0b4c57ab2e27
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2024-05-09 16:44:28 -0300

    refactor(TextField): add one prop for component and make a litle kludge for don't show errors on console, i make this kludge because this component is static it's don't work

--- src/components/textFieldOutline.tsx
+++ src/components/textFieldOutline.tsx
@@ -1,15 +1,16 @@
-import React, { useState } from 'react';
+import React, { ComponentProps, useEffect, useState } from 'react';
 import TextField from '@mui/material/TextField';
 import InputAdornment from '@mui/material/InputAdornment';
 import IconButton from '@mui/material/IconButton';
 
-interface ITextFielProps{
+interface ITextFielProps extends ComponentProps<"input">{
     label:string
     defaultValue?:string
     focused?:boolean
+
 }
 
-const MyTextField:React.FC<ITextFielProps> = ({defaultValue, label, focused}) => {
+const MyTextField:React.FC<ITextFielProps> = ({defaultValue, label, focused, value:valueProp}) => {
   const [value, setValue] = useState(defaultValue);
   const [isFocused, setIsFocused] = useState(false);
 
@@ -21,11 +22,14 @@ const MyTextField:React.FC<ITextFielProps> = ({defaultValue, label, focused}) =>
     setValue('');
   };
 
+  useEffect(() => {
+    setValue(valueProp as string);
+  }, [valueProp])
   return (
     <TextField
       label={label}
       InputLabelProps={{shrink: true}}
-      value={value}
+      value={!defaultValue ? value ?? "" : defaultValue}
       onChange={handleChange}
       variant="outlined"
       focused={focused}
