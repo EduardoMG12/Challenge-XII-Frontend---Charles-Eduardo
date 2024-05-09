4aa29a98bc2217986bbe34e549f9c8f052cf7c42
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2024-05-09 13:58:01 -0300

    feat: add responve with screen widths of 1440px and 414px, as specified in the project requirements on Notion to home part 1

--- src/components/sectionFindRide/index.tsx
+++ src/components/sectionFindRide/index.tsx
@@ -2,7 +2,7 @@ import MyTextField from "../textFieldOutline";
 
 const SectionFindRide = () => {
     return (
-        <div className="w-full h-[46.36rem] px-24 flex justify-between items-center">
+        <div className="w-full md:h-[46.36rem] px-4 md:px-24 flex flex-col mt-20 md:mt-0 md:flex-row justify-between items-center">
             <img
                 src="https://challengeuolpbcompass.s3.amazonaws.com/Home/imageFirstSectionFindRide.png"
                 alt=""
@@ -10,17 +10,17 @@ const SectionFindRide = () => {
             />
             <div>
                 <div>
-                    <h2 className="text-2xl text-white">NEED A RIDE?</h2>
-                    <h2 className="text-5xl text-white flex mb-7">
-                        Book with{" "}
-                        <span className="text-5xl text-[#FBA403]">
+                    <h2 className="text-xl md:text-2xl text-white mt-7 md:mt-0">NEED A RIDE?</h2>
+                    <h2 className="text-4xl md:text-5xl text-white flex mb-7">
+                        Book with
+                        <span className="text-4xl md:text-5xl text-[#FBA403]">
                             &nbsp;myRIDE&nbsp;
                         </span>{" "}
                         now!
                     </h2>
                     <form
                         action=""
-                        className="gap-4 flex flex-col w-[38.125rem] bg-[#2C2C2C] py-[1.875rem] px-8"
+                        className="gap-4 flex flex-col md:w-[38.125rem] bg-[#2C2C2C] py-[1.875rem] px-8 mx-4 md:mx-0"
                     >
                         <p className="text-2xl text-white">Find a ride now</p>
                         <MyTextField
@@ -29,7 +29,7 @@ const SectionFindRide = () => {
                         />
                         <MyTextField label="Your Destination" focused />
                         <div className="flex items-center justify-between">
-                            <button type="button" className="rounded-[0.25rem] flex items-center gap-1 bg-[#FBA403] px-7 p-4 text-white">
+                            <button type="button" className="rounded-[0.25rem] flex items-center gap-1 bg-[#FBA403] px-2 py-5 md:px-7 md:py-4 text-white">
                                 <img src="https://challengeuolpbcompass.s3.amazonaws.com/Home/lupe.svg" alt="" /> FIND A DRIVER
                             </button>
                             <p className="text-[#FBA403]">MORE OPTIONS</p>
