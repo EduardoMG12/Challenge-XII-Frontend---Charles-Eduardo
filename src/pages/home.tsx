fdbef7b1d08ec321d315b44cb2654207e02d2474
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2024-05-09 19:32:37 -0300

    feat: implement home part 2

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,11 @@
+import SectionClassRides from "../components/sectionClassRides";
 import SectionFindRide from "../components/sectionFindRide";
 
 const Home = () => {
   return (
     <div className='min-h-dvh min-w-full max-w-full'>
         <SectionFindRide />
-
+        <SectionClassRides />
 
     </div>
   )
