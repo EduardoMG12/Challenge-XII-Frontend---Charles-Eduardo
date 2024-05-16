4b2a796ffbd2ba858b460f60eb4a64faf904547f
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2024-05-16 15:01:29 -0300

    feat: apply font Roboto and adjust height modal select

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -6,48 +6,60 @@ export const theme = createTheme({
             main: "#FBA403",
         },
     },
+    typography: {
+        fontFamily: "Roboto, sans-serif",
+    },
     components: {
         MuiOutlinedInput: {
             styleOverrides: {
                 root: {
-                    '& .MuiInputBase-input': {
-                        color: '#FFFFFF', 
-                      },
-                      '& .MuiOutlinedInput-notchedOutline': {
-                        borderColor: '#FFFFFF', 
-                      },
-                      '&:hover .MuiInputBase-input': {
-                        color: '#FBA403', 
-                      },
-                      '&:hover .MuiOutlinedInput-notchedOutline': {
-                        borderColor: '#FBA403', 
-                      },
-                      '&.Mui-focused .MuiInputBase-input': {
-                        color: '#FBA403', 
-                      },
+                    "& .MuiInputBase-input": {
+                        color: "#FFFFFF",
+                    },
+                    "& .MuiOutlinedInput-notchedOutline": {
+                        borderColor: "#FFFFFF",
+                    },
+                    "&:hover .MuiInputBase-input": {
+                        color: "#FBA403",
+                    },
+                    "&:hover .MuiOutlinedInput-notchedOutline": {
+                        borderColor: "#FBA403",
+                    },
+                    "&.Mui-focused .MuiInputBase-input": {
+                        color: "#FBA403",
+                    },
                 },
             },
         },
         MuiFormLabel: {
             styleOverrides: {
-              root: {
-                color:"#FFFFFF",
-                '&:hover .MuiInputLabel-shrink': {
-                    color:"blue"
+                root: {
+                    color: "#FFFFFF",
+                    "&:hover .MuiInputLabel-shrink": {
+                        color: "blue",
+                    },
+                    "&.Mui-focused": {
+                        color: "#FBA403",
+                    },
+                    "&.MuiInputLabel-root:hover": {
+                        color: "#FBA403",
+                    },
                 },
-                '&.Mui-focused': {
-                  color: '#FBA403',
-                },
-                '&.MuiInputLabel-root:hover': {
-                    color: '#FBA403',
-                  },
-              },
             },
-          },
-          MuiSelect: {
+        },
+        MuiSelect: {
             styleOverrides: {
                 icon: {
-                    color: '#FFFFFF', // Substitua 'cor_desejada' pela cor que você quer.
+                    color: "#FFFFFF",
+                },
+            },
+        },
+        MuiMenu: {
+            styleOverrides: {
+                paper: {
+                    maxHeight: "6.25rem",
+                    backgroundColor: "#383838",
+                    color: "#FFFFFF",
                 },
             },
         },
