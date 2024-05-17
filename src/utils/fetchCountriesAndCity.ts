78f237b92208902dca4dc2eb41edc71a3fff69f8
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2024-05-16 22:02:40 -0300

    feat: add fetch to get cities data

---
+++ src/utils/fetchCountriesAndCity.ts
@@ -0,0 +1,56 @@
+import axios from "axios";
+
+export interface Countries {
+    error: boolean;
+    msg: string;
+    data: Daum[];
+}
+
+export interface Daum {
+    iso2: string;
+    iso3: string;
+    country: string;
+    cities: string[];
+}
+
+export function fetchCountries(
+    setCountries: React.Dispatch<React.SetStateAction<string[] | undefined>>
+) {
+    axios
+        .get<Countries>("https://countriesnow.space/api/v0.1/countries")
+        .then((response) => {
+            const sortedCountries = response.data.data.sort((a, b) =>
+                a.country.localeCompare(b.country)
+            );
+            const countryNames = sortedCountries.map(
+                (country) => country.country
+            );
+            setCountries(countryNames);
+        })
+        .catch((error) => {
+            console.error("Error find countries:", error);
+        });
+}
+
+export interface Cities {
+    error: boolean;
+    msg: string;
+    data: string[];
+}
+
+export function fetchCities(
+    countryName: string,
+    setCities: React.Dispatch<React.SetStateAction<string[] | undefined>>
+) {
+    axios
+        .post<Cities>("https://countriesnow.space/api/v0.1/countries/cities", {
+            country: countryName,
+        })
+        .then((response) => {
+            console.log(response)
+            setCities(response.data.data);
+        })
+        .catch((error) => {
+            console.error("Error find cities:", error);
+        });
+}
