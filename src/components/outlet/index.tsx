c0bf5955ccf52d6afcbdb2e44e0cebdb1b42e7bb
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2025-02-22 16:03:18 -0300

    feat:create top header with message for redirect to /README page

--- src/components/outlet/index.tsx
+++ src/components/outlet/index.tsx
@@ -1,10 +1,12 @@
 import Footer from "./footer";
 import Header from "./header";
 import { Outlet as RouterDomOutlet } from "react-router-dom";
+import MessageBar from "./messageBar";
 
 const Outlet = () => {
     return (
         <>
+            <MessageBar />
             <Header />
             <RouterDomOutlet />
             <Footer />
---
+++ src/components/outlet/messageBar.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { XIcon } from "../../icons/XIcon";
+import MaximizeIcon from "../../icons/maximize";
+
+export default function MessageBar() {
+    const [isMinimized, setIsMinimized] = useState(false);
+
+    return (
+        <div
+            className={`bg-[#FBA403] fixed top-0 right-0 flex items-center pl-4 ${isMinimized ? "pr-2" : "pr-4"} transition-all duration-300 overflow-hidden ${isMinimized ? "h-8 w-12 justify-end" : "h-6 w-full justify-between"
+                }`}
+        >
+            {!isMinimized && (
+                <a href="/README">
+                    <p className="text-white text-lg transition-opacity duration-300">
+                        🚀 Confira a documentação completa do projeto e veja todos os detalhes! 🚀
+                    </p>
+                </a>
+            )}
+            <div className="flex justify-center items-center w-10">
+                {isMinimized ?
+
+                    <MaximizeIcon onClick={() => setIsMinimized(!isMinimized)} /> :
+
+
+                    <XIcon onClick={() => setIsMinimized(!isMinimized)} />
+                }
+            </div>
+        </div>
+    );
+}
