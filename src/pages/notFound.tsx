df60fdc6c0408ace4d64a507d7ddf8519292f3e5
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2025-02-22 16:03:35 -0300

    fix: fix icon on page not found

--- src/pages/notFound.tsx
+++ src/pages/notFound.tsx
@@ -1,14 +1,12 @@
+import Attention from "../icons/Attention";
+
 const NotFound = () => {
     return (
         <div
             className="w-full h-screen flex flex-col justify-center items-center"
             style={{ height: `calc(100vh - 360px)` }}
         >
-            <img
-                src="https://challengeuolpbcompass.s3.amazonaws.com/icon404NotFound.svg"
-                alt="Icon with a hollowed-out design featuring an orange circular border and an orange exclamation point in the center"
-                className="w-40"
-            />
+            <Attention />
         </div>
     );
 };
